c9e781ed974077ac6ab54d5ad77cbb407d87c2dc
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2022-03-23 14:54:22 +0700

    create project and add gitignore file

---
+++ SwiftUI-Social-App/SwiftUI-Social-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Social-App
+//
+//  Created by Le Hoang Anh on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Social-App/SwiftUI-Social-App/SwiftUI_Social_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Social_AppApp.swift
+//  SwiftUI-Social-App
+//
+//  Created by Le Hoang Anh on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Social_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
